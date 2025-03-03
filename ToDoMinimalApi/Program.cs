a21abd3e727258fbb9a228f79ee887ba60d48386
Farah Alhaddad <Farah.alhaddad@hotmail.com>
2025-03-03 20:34:39 +0100

    Initial commit.

---
+++ ToDoMinimalApi/Program.cs
@@ -0,0 +1,15 @@
+namespace ToDoMinimalApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
